f53ca8f3ea5a99a7848c88e6b4674d81612bb0d8
todik panshin <todikp@gmail,com>
2023-05-01 02:08:47 +0300

    fix img

--- js/about.controller.js
+++ js/about.controller.js
@@ -3,6 +3,6 @@
 function renderAboutTxt(){
  
     const strHtmls=`<p>${makeLorem()}</p>
-    <img class="sleepy" src="/img/sleepy.jpg" alt="">`
+    <img class="sleepy" src="img/sleepy.jpg" alt="">`
     document.querySelector('.about-text').innerHTML = strHtmls
 }
